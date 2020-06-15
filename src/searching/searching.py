26c9a043adf6a6d8159e1809940fd5e475f81e4a
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-15 19:11:28 -0400

    Implemented linear_search in searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,10 @@
 def linear_search(arr, target):
     # Your code here
-
+    current_index = 0
+    while current_index < len(arr):
+        if arr[current_index] == target:
+            return current_index
+        current_index += 1
 
     return -1   # not found
 
@@ -12,12 +16,10 @@ def binary_search(arr, target):
     # variables to store the min and max of the remaining searchable portion of array
     min_position = 0
     max_position = len(arr)
-    print(f"min: {min_position}, max: {max_position}")
     # Iterates through search until it finds the value
     for _ in range(0, (len(arr)//2)):
         # Variable to store the currently tested position in the array
         new_position = (max_position + min_position) // 2
-        print(f"new position: {new_position}")
 
         # If the value at new_position is the target, return the value
         if arr[new_position] == target:
@@ -26,11 +28,9 @@ def binary_search(arr, target):
         # Otherwise, if the value is less than the target, set min to new position for the next loop
         elif arr[new_position] < target:
             min_position = new_position
-            print(f"min: {min_position}, max: {max_position}")
 
         # lastly, if value is greater than the target, set max to new position for the next loop
         else:
             max_position = new_position
-            print(f"min: {min_position}, max: {max_position}")
 
     return -1  # not found
